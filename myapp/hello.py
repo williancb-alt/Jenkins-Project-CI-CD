e1c75d19678325a8b214ddad1af74c9a1c612d76
L00203060 <L00203060@atu.ie>
2025-11-23 00:37:09 +0000

    trying again

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,7 +1,10 @@
-import fire 
+import argparse
 
 def hello(name="World"):
-  return "Hello %s!" % name
+    return "Hello %s!" % name
 
 if __name__ == '__main__':
-  fire.Fire(hello)
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('--name', default='World', help='Name to greet')
+    args = parser.parse_args()
+    print(hello(args.name))
